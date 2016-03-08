3b4ce4d06eb31c901f3b275c003eacbbb32aa5c3
Ye Li <ye.li@nxp.com>
2016-03-08 22:03:47 +0800

    MLK-12500-1 HAB: Add kernel image authentication in image loading
    To support the trust boot chain, we integrate the authentication
    into the kernel image loading process. The kernel image will be verified
    at its load address. So when signing the kernel image, we need to
    use this load address which may change on different platforms.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit 3c118b8d6bbe1a25ca8c8bafeb528309f16fc73d)
    (cherry picked from commit fd9a9759ed9b3a9fc26b18aff00880382213b1ca)
    (cherry picked from commit 98d4faefdb83579d4a5a170e06af5efb64ad2b3c)
    (cherry picked from commit 3c0f0eed6cb703cd7d67ec97520b1990b5e35fb2)
    (cherry picked from commit 6605ea47d7fbbd4687c8eb636335f4c1165be4e4)
    (cherry picked from commit 6391ea5549199bff43a31c923593624f1c55e663)

--- cmd/bootz.c
+++ cmd/bootz.c
@@ -68,6 +68,14 @@ static int bootz_start(struct cmd_tbl *cmdtp, int flag, int argc,
 			      zi_end - zi_start))
 		return 1;
 
+#ifdef CONFIG_IMX_HAB
+	extern int authenticate_image(
+			uint32_t ddr_start, uint32_t raw_image_size);
+	if (authenticate_image(images->ep, zi_end - zi_start) != 0) {
+		printf("Authenticate zImage Fail, Please check\n");
+		return 1;
+	}
+#endif
 	return 0;
 }
 
