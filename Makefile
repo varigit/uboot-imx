varigit/uboot-imx

commit 8cb6612516b9909ebbb9414cd800bb2f84ce332e

MA-7455-2 Workaround to pass compile for pico
We have some vendor specific codes in board/freescale/common
which the picoimx configs also need them when Android or
Brillo configs is enabled.
So added the folder in Makefile to pass the compile.

Signed-off-by: Haoran Wang <Haoran.Wang@freescale.com>

Makefile
@@ -624,6 +624,7 @@ HAVE_VENDOR_COMMON_LIB = $(if $(wildcard $(srctree)/board/$(VENDOR)/common/Makef
 
 libs-y += lib/
 libs-$(HAVE_VENDOR_COMMON_LIB) += board/$(VENDOR)/common/
+libs-$(CONFIG_FSL_FASTBOOT) += board/freescale/common/
 libs-$(CONFIG_OF_EMBED) += dts/
 libs-y += fs/
 libs-y += net/

commit abfc6dc23b0e697e88c191ef95e95d24cb2593b4

Workaround for issue caused by commit 8cb6612516b9909ebbb9414cd800bb2f84ce332e The above commit was a workaround for a specific board, but it affected every board that uses CONFIG_FSL_FASTBOOT. This commit fixes this by applying the original workaround only for its specific board.

Makefile
@@ -624,7 +624,9 @@ HAVE_VENDOR_COMMON_LIB = $(if $(wildcard $(srctree)/board/$(VENDOR)/common/Makef
 
 libs-y += lib/
 libs-$(HAVE_VENDOR_COMMON_LIB) += board/$(VENDOR)/common/
+ifeq ($(CONFIG_TARGET_PICOSOM_IMX6UL),y)
 libs-$(CONFIG_FSL_FASTBOOT) += board/freescale/common/
+endif
 libs-$(CONFIG_OF_EMBED) += dts/
 libs-y += fs/
 libs-y += net/