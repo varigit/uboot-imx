404c3c0618cd484fb319b8a8bc4cba8573b103e1
Konstantin Sinyuk <konstantin.s@variscite.com>
2017-03-26 13:58:09 +0300

    fw_env: fix compilation error under gcc6
    Include stdint.h unconditionally to fix the "unknown type name 'uint8_t'" error

--- tools/env/fw_env.c
+++ tools/env/fw_env.c
@@ -17,6 +17,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <stdint.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
@@ -24,7 +25,6 @@
 #include <unistd.h>
 
 #ifdef MTD_OLD
-# include <stdint.h>
 # include <linux/mtd/mtd.h>
 #else
 # define  __user	/* nothing */
