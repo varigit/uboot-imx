6ff20a2b0712395a1a2bbcb80e50b28e35b4512f
varigigi <pierluigi.p@variscite.com>
2017-05-13 16:31:02 +0200

    introduce runtime soc_type helper for imx6 and imx7

--- arch/arm/include/asm/arch-mx7/sys_proto.h
+++ arch/arm/include/asm/arch-mx7/sys_proto.h
@@ -16,9 +16,16 @@ u32 get_cpu_rev(void);
 
 /* returns MXC_CPU_ value */
 #define cpu_type(rev) (((rev) >> 12)&0xff)
+#define soc_type(rev) (((rev) >> 12)&0xf0)
 
 /* use with MXC_CPU_ constants */
-#define is_cpu_type(cpu) (cpu_type(get_cpu_rev()) == cpu)
+#define get_cpu_type()	(cpu_type(get_cpu_rev()))
+#define get_soc_type()	(soc_type(get_cpu_rev()))
+#define is_cpu_type(cpu) (get_cpu_type() == cpu)
+#define is_soc_type(soc) (get_soc_type() == soc)
+
+#define is_mx6() (is_soc_type(MXC_SOC_MX6))
+#define is_mx7() (is_soc_type(MXC_SOC_MX7))
 
 const char *get_imx_type(u32 imxtype);
 unsigned imx_ddr_size(void);
