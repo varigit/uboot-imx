af0f6b138af8d0816b87b00486b945d78056fe00
Eran Matityahu <eran.m@variscite.com>
2017-11-29 10:53:39 +0200

    mx6var_som: Move the setup_display() call from board_early_init_f() to board_init()
    This fixes the rare issue of a blank white screen and an
    "mxc_sdc_fb fb@0: timeout when waiting for flip irq"
    error in the kernel.
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- board/variscite/mx6var_som/mx6var_som.c
+++ board/variscite/mx6var_som/mx6var_som.c
@@ -1103,9 +1103,6 @@ int board_ehci_power(int port, int on)
 int board_early_init_f(void)
 {
 	setup_iomux_uart();
-#if defined(CONFIG_VIDEO_IPUV3)
-	setup_display();
-#endif
 #ifdef CONFIG_SYS_I2C_MXC
 	setup_local_i2c();
 #endif
@@ -1118,7 +1115,9 @@ int board_early_init_f(void)
 
 int board_init(void)
 {
-	int ret = 0;
+#if defined(CONFIG_VIDEO_IPUV3)
+	setup_display();
+#endif
 
 	/* address of boot parameters */
 	gd->bd->bi_boot_params = CONFIG_SYS_SDRAM_BASE + 0x100;
@@ -1127,7 +1126,7 @@ int board_init(void)
 #ifdef CONFIG_USB_EHCI_MX6
 	setup_usb();
 #endif
-	return ret;
+	return 0;
 }
 
 static struct pmic *pfuze;
