3d0c1d244fed25d3d35c2c458e9d14621d46a952
Eran Matityahu <eran.m@variscite.com>
2018-03-06 16:08:56 +0200

    autoboot: SDP: Stop setting the environment to default when booting from USB
    Stop setting the environment to default when booting from USB,
    so the runtime environment variables will be kept.
    
    Signed-off-by: Eran Matityahu <eran.m@variscite.com>

--- common/autoboot.c
+++ common/autoboot.c
@@ -316,8 +316,6 @@ const char *bootdelay_process(void)
 		disconnect_from_pc();
 		printf("Boot from USB for mfgtools\n");
 		bootdelay = 0;
-		set_default_env("Use default environment for \
-				 mfgtools\n");
 	} else if (is_boot_from_usb()) {
 		printf("Boot from USB for uuu\n");
 		env_set("bootcmd", "fastboot 0");
