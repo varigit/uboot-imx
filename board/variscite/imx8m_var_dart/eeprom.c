55465fb4fc2310c5987d287aac7310403de7bae0
Felix Radensky <felix.r@variscite.com>
2018-05-08 18:01:11 +0300

    DART-MX8M: eeprom: Reworked EEPROM layout
    Changed field sizes, added eeprom revision field.
    Added routine to print SOM production info.
    Compile EEPROM code only for u-boot, not SPL

--- board/variscite/imx8m_var_dart/eeprom.c
+++ board/variscite/imx8m_var_dart/eeprom.c
@@ -5,23 +5,28 @@
  */
 
 #include <common.h>
+#include <command.h>
 #include <dm.h>
 #include <i2c.h>
 
 #include "eeprom.h"
 
-static struct udevice *eeprom_dev = NULL;
+static struct var_eeprom e;
+static int eeprom_is_valid = 0;
+static int eeprom_was_read = 0;
+static struct udevice *edev = NULL;
 
+/**
+ * read_eeprom - read the EEPROM into memory
+ */
 static int var_eeprom_init(void)
 {
 	int ret;
-	u16 magic;
 	int i2c_bus = VAR_EEPROM_I2C_BUS;
 	uint8_t chip = VAR_EEPROM_I2C_ADDR;
 	struct udevice *bus, *dev;
-	u32 offset = offsetof(struct var_eeprom, magic);
 
-	if (eeprom_dev)
+	if (edev)
 		return 0;
 
 	ret = uclass_get_device_by_seq(UCLASS_I2C, i2c_bus, &bus);
@@ -37,48 +42,110 @@ static int var_eeprom_init(void)
 		return ret;
 	}
 
-	ret = dm_i2c_read(dev, offset, (u8 *)&magic, sizeof(magic));
-	if (ret) {
-		eeprom_debug("I2C read failed for bus %d addr %d\n",
-			VAR_EEPROM_I2C_BUS, VAR_EEPROM_I2C_ADDR);
-		return ret;
-	}
-
-	if (magic != VAR_EEPROM_MAGIC) {
-		eeprom_debug("Invalid EEPROM magic 0x%x, expected 0x%x",
-			magic, VAR_EEPROM_MAGIC);
-		return -1;
-	}
-
-	eeprom_dev = dev;
+	edev = dev;
 
 	return 0;
 }
 
-
-static int var_eeprom_read(u32 offset, u8 *buf, int len)
+/**
+ * read_eeprom - read the EEPROM into memory
+ */
+static int var_read_eeprom(void)
 {
 	int ret;
 
+	/* EEPROM was read to memory and validated */
+	if (eeprom_is_valid)
+		return 0;
+
+	/* EEPROM was read to memory and validation failed */
+	if (eeprom_was_read)
+		return -1;
+
+	/* Initialize EEPROM */
 	ret = var_eeprom_init();
 	if (ret)
 		return ret;
 
-	return dm_i2c_read(eeprom_dev, offset, buf, len);
+	/* Read EEPROM to memory */
+	ret = dm_i2c_read(edev, 0, (void *)&e, sizeof(e));
+	if (ret)
+		return ret;
+
+	/* Mark EEPROM as read */
+	eeprom_was_read = 1;
+
+	if (htons(e.magic) != VAR_EEPROM_MAGIC) {
+		eeprom_debug("Invalid EEPROM magic 0x%hx, expected 0x%hx\n",
+			htons(e.magic), VAR_EEPROM_MAGIC);
+		return -1;
+	}
+
+	/* Mark EEPROM as valid */
+	eeprom_is_valid = 1;
+
+	return 0;
 }
 
 int var_eeprom_read_mac(u8 *buf)
 {
-	u32 offset = offsetof(struct var_eeprom, mac);
+	int ret;
 
-	return var_eeprom_read(offset, buf, 6);
+	ret = var_read_eeprom();
+	if (ret)
+		return ret;
+
+	memcpy(buf, e.mac, sizeof(e.mac));
+
+	return 0;
 }
 
 int var_eeprom_read_dram_size(u8 *buf)
 {
-	u32 offset = offsetof(struct var_eeprom, dram_size);
+	int ret;
 
-	return var_eeprom_read(offset, buf, 1);
+	ret = var_read_eeprom();
+	if (ret)
+		return ret;
+
+	memcpy(buf, (void* )&e.rs, sizeof(e.rs));
+
+	return 0;
+}
+
+void var_eeprom_print_info(void)
+{
+	int ret;
+
+	ret = var_read_eeprom();
+	if (ret)
+		return;
+
+	printf("\nPart number: VSM-DT8M-%.*s\n", (int)sizeof(e.pn), (char *)e.pn);
+
+	printf("Assembly: AS%.*s\n", (int)sizeof(e.as), (char *)e.as);
+
+	printf("Production date: %.*s %.*s %.*s\n",
+			4, /* YYYY */
+			(char *)e.date,
+			3, /* MMM */
+			((char *)e.date) + 4,
+			2, /* DD */
+			((char *)e.date) + 4 + 3);
+
+	switch (e.sr) {
+	case 0:
+		printf("SOM revision: 1.1\n\n");
+		break;
+	default:
+		printf("SOM revision: unknown\n\n");
+	}
+
+#ifdef EEPROM_DEBUG
+	printf("EEPROM version: 0x%x\n", e.ver);
+	printf("SOM options: 0x%x\n", e.opt);
+	printf("DRAM size: %dGiB\n\n", e.rs);
+#endif
 }
 
 
--- board/variscite/imx8m_var_dart/eeprom.h
+++ board/variscite/imx8m_var_dart/eeprom.h
@@ -11,12 +11,16 @@
 
 #define VAR_EEPROM_I2C_BUS	0
 #define VAR_EEPROM_I2C_ADDR	0x52
+#define VAR_EEPROM_PAGE_SIZE	16
 
-/* Bits of "features" field in the eeprom structure below */
+/* Optional SOM features */
 #define VAR_EEPROM_F_WIFI 	(1 << 0)
 #define VAR_EEPROM_F_ETH 	(1 << 1)
 #define VAR_EEPROM_F_AUDIO 	(1 << 2)
 #define VAR_EEPROM_F_LVDS	(1 << 3)
+
+/* SOM revision numbers */
+#define SOM_REV_1_1		0
 
 #ifdef EEPROM_DEBUG
 #define eeprom_debug(M, ...) printf("EEPROM_DEBUG: " M, ##__VA_ARGS__)
@@ -24,20 +28,21 @@
 #define eeprom_debug(M, ...)
 #endif
 
-
 struct __attribute__((packed)) var_eeprom
 {
-	u16 magic;
-	u8 partnum[8];
-	u8 assembly[11];
-	u8 date[9];
-	u8 mac[6];
-	u8 revision;
-	u8 features;
-	u8 dram_size;
+	u16 magic;	/* 00-0x00 - magic number    -	8M	        */
+	u8 pn[3];	/* 02-0x02 - part number     -	VSM-DT8M-001    */
+	u8 as[10];	/* 05-0x05 - assembly        -	AS1803138551    */
+	u8 date[9];	/* 15-0x0f - build date      -	YYYYMMMDD       */
+	u8 mac[6];	/* 24-0x18 - MAC address     -	000102030405    */
+	u8 sr;		/* 30-0x1e - SOM revision    -	01              */
+	u8 ver;		/* 31-0x1f - EEPROM version  -	01              */
+	u8 opt;		/* 32-0x20 - SOM features    -	0f              */
+	u8 rs;		/* 33-0x21 - RAM size in GB  -	01              */
 };
 
 extern int var_eeprom_read_mac(u8 *buf);
 extern int var_eeprom_read_dram_size(u8 *buf);
+extern void var_eeprom_print_info(void);
 
 #endif /* _MX8M_VAR_EEPROM_H_ */
