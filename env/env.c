734b5e6ef07ac8f0a46b86abc96e48e347194ae8
Sam Protsenko <semen.protsenko@linaro.org>
2018-07-30 19:19:26 +0300

    env: Don't show "Failed" error message
    "Failed" error message from env_load() only clutters the log with
    unnecessary details, as we already have all needed warnings by that
    time. Example:
    
        Loading Environment from FAT... MMC: no card present
        ** Bad device mmc 0 **
        Failed (-5)
    
    Let's only print it in case when DEBUG is defined to keep log clear.
    
    Signed-off-by: Sam Protsenko <semen.protsenko@linaro.org>

--- env/env.c
+++ env/env.c
@@ -179,13 +179,17 @@ int env_load(void)
 
 		printf("Loading Environment from %s... ", drv->name);
 		ret = drv->load();
-		if (ret)
-			printf("Failed (%d)\n", ret);
-		else
+		/*
+		 * In error case, the error message must be printed during
+		 * drv->load() in some underlying API, and it must be exactly
+		 * one message.
+		 */
+		if (ret) {
+			debug("Failed (%d)\n", ret);
+		} else {
 			printf("OK\n");
-
-		if (!ret)
 			return 0;
+		}
 	}
 
 	return -ENODEV;
