1c03acdd8fbc51f3cb860dc8bf09e0bc6e032c07
Felix Radensky <felix.r@variscite.com>
2019-05-01 19:08:46 +0300

    imx8m_var_dart: Disable CONFIG_DM_I2C and CONFIG_DM_PMIC
    PMIC code is used only by SPL which disables CONFIG_DM_I2C anyway.
    CONFIG_DM_I2C (required by CONFIG_DM_PMIC) prevents EEPROM code
    from being used at early boot stages.
    
    This a preparation step for upcoming DDR EEPROM code.

--- include/configs/imx8m_var_dart.h
+++ include/configs/imx8m_var_dart.h
@@ -47,13 +47,6 @@
 #define CONFIG_SPL_ABORT_ON_RAW_IMAGE /* For RAW image gives a error info not panic */
 
 #undef CONFIG_DM_MMC
-#undef CONFIG_DM_PMIC
-#undef CONFIG_DM_PMIC_PFUZE100
-
-#define CONFIG_SYS_I2C
-#define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
-#define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
-#define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
 
 #define CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG
 
@@ -62,6 +55,11 @@
 #define CONFIG_POWER_PFUZE100
 #define CONFIG_POWER_PFUZE100_I2C_ADDR 0x08
 #endif
+
+#define CONFIG_SYS_I2C
+#define CONFIG_SYS_I2C_MXC_I2C1		/* enable I2C bus 1 */
+#define CONFIG_SYS_I2C_MXC_I2C2		/* enable I2C bus 2 */
+#define CONFIG_SYS_I2C_MXC_I2C3		/* enable I2C bus 3 */
 
 #define CONFIG_REMAKE_ELF
 
