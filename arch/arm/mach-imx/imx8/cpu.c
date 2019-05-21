038cda339aaea521e93fc2e9900be9645bb62b1b
Ye Li <ye.li@nxp.com>
2019-05-20 20:35:26 -0700

    MLK-19250 imx: uuu: Decouple mmc_get_env_dev with CONFIG_ENV_IS_IN_MMC
    For flash devices, like spi nor, weim nor and qspi/flexspi,
    we don't define the ENV in MMC, but UUU fastboot always use the
    mmc_get_env_dev. So we have to decouple the function with
    CONFIG_ENV_IS_IN_MMC
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit ef480647da5686403c7fed09aaf47ca1ace54731)
    (cherry picked from commit 368e256abf1950a590c8b28badd6a1997a6ea454)
    (cherry picked from commit 34ddce8dbb462b622e172b62dd2620485dce6306)
    (cherry picked from commit 80913e7f20b4761875c3108609d510879bc710ef)

--- arch/arm/mach-imx/imx8/cpu.c
+++ arch/arm/mach-imx/imx8/cpu.c
@@ -437,7 +437,6 @@ void get_board_serial(struct tag_serialnr *serialnr)
 }
 #endif /*CONFIG_ENV_VARS_UBOOT_RUNTIME_CONFIG*/
 
-#ifdef CONFIG_ENV_IS_IN_MMC
 __weak int board_mmc_get_env_dev(int devno)
 {
 	return devno;
@@ -467,7 +466,6 @@ int mmc_get_env_dev(void)
 
 	return board_mmc_get_env_dev(devno);
 }
-#endif
 
 #define MEMSTART_ALIGNMENT  SZ_2M /* Align the memory start with 2MB */
 
