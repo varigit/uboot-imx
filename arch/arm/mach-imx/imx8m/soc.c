bb559e65b3720b8f2c4b5eda576b53b3b885cd9b
Felix Radensky <felix.r@variscite.com>
2020-08-12 17:42:40 +0300

    imx8m: dram: Allow overriding DRAM init routines in board code

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -197,6 +197,7 @@ void enable_caches(void)
 	dcache_enable();
 }
 
+#ifndef CONFIG_IMX8M_BOARD_INIT_DRAM
 __weak int board_phys_sdram_size(phys_size_t *size)
 {
 	if (!size)
@@ -282,6 +283,7 @@ phys_size_t get_effective_memsize(void)
 	return gd->ram_size;
 #endif
 }
+#endif
 
 static u32 get_cpu_variant_type(u32 type)
 {
