d951030fcffbdea7db03b1b407fb72d973b78eb0
FrancescoFerraro <francesco.f@variscite.com>
2021-11-18 10:44:50 +0100

    imx8m: soc.c: fixed duplicate definition of get_effective_memsize()
    Moved phys_size_t get_effective_memsize(void) under
          #ifndef CONFIG_IMX8M_BOARD_INIT_DRAM
    This fixes compilation error: multiply definition of get_effective_memsize
    
    Cherry-picked from 8ae6c764bb08694ecb154672fe24daf7491b457c
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- arch/arm/mach-imx/imx8m/soc.c
+++ arch/arm/mach-imx/imx8m/soc.c
@@ -320,7 +320,6 @@ int dram_init_banksize(void)
 
 	return 0;
 }
-#endif
 
 phys_size_t get_effective_memsize(void)
 {
@@ -350,6 +349,7 @@ phys_size_t get_effective_memsize(void)
 		return PHYS_SDRAM_SIZE;
 	}
 }
+#endif
 
 static u32 get_cpu_variant_type(u32 type)
 {
