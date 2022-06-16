1ca029c9cf9597d9078d413c4627662ae1e01649
Nate Drude <nate.d@variscite.com>
2022-06-16 10:08:19 -0500

    imx8mn-var-som: Fix invalid SD device for uuu
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- include/configs/imx8mn_var_som.h
+++ include/configs/imx8mn_var_som.h
@@ -100,7 +100,7 @@
 	"initrd_addr=0x43800000\0" \
 	"initrd_high=0xffffffffffffffff\0" \
 	"emmc_dev=2\0" \
-	"sd_dev=0\0" \
+	"sd_dev=1\0" \
 
 /* Initial environment variables */
 #define CONFIG_EXTRA_ENV_SETTINGS \
