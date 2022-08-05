c5b964b31f000c1ccff9202476ccdc2b93afc640
FrancescoFerraro <francesco.f@variscite.com>
2022-08-05 10:37:42 +0200

    imx8mn_var_som: support distro_bootcmd
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- include/configs/imx8mn_var_som.h
+++ include/configs/imx8mn_var_som.h
@@ -66,6 +66,22 @@
 #endif
 #endif
 
+#ifdef CONFIG_DISTRO_DEFAULTS
+#define BOOT_TARGET_DEVICES(func) \
+	func(USB, usb, 0) \
+	func(MMC, mmc, 1) \
+	func(MMC, mmc, 2)
+
+#include <config_distro_bootcmd.h>
+/* redefine BOOTENV_EFI_SET_FDTFILE_FALLBACK to use Variscite function to load fdt */
+#undef BOOTENV_EFI_SET_FDTFILE_FALLBACK
+#define BOOTENV_EFI_SET_FDTFILE_FALLBACK \
+	"setenv efi_dtb_prefixes; " \
+	"run loadfdt; "
+#else
+#define BOOTENV
+#endif
+
 /* UUU environment variables */
 #define CONFIG_MFG_ENV_SETTINGS \
 	CONFIG_MFG_ENV_SETTINGS_DEFAULT \
@@ -78,6 +94,7 @@
 #define CONFIG_EXTRA_ENV_SETTINGS \
 	CONFIG_MFG_ENV_SETTINGS \
 	"bootdir=/boot\0" \
+	BOOTENV \
 	"scriptaddr=0x43500000\0" \
 	"kernel_addr_r=" __stringify(CONFIG_LOADADDR) "\0" \
 	"bsp_script=boot.scr\0" \
@@ -99,6 +116,7 @@
 	"m7_addr=0x7e0000\0" \
 	"m7_bin=hello_world.bin\0" \
 	"use_m7=no\0" \
+	"dfu_alt_info=mmc 2=1 raw 0x40 0x1000 mmcpart\0" \
 	"loadm7bin=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${m7_bin} && " \
 	"cp.b ${loadaddr} ${m7_addr} ${filesize}\0" \
 	"runm7bin=" \
