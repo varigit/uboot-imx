394a8918554a1c410773d766c88cc45bd7985c43
Nate Drude <nate.d@variscite.com>
2023-02-20 11:07:34 -0600

    variscite: imx9_eeprom: Add support for DDR configuration from EEPROM
    This adds the SPL functions needed to configure DDR parameters for
    Variscite's different DDR configurations. A base configuration is
    compiled into SPL (lpddr4x_timing.c) and a diff of these values is
    stored in EEPROM. At runtime, these functions determine whether any
    re-configuration is required and apply as needed to the default timing.
    
    This change was designed around DDR configuration data produced by the
    i.MX Version 13.1 of the NXP configuration tool. Furthermore, this
    change assumes that x3 frequency setpoints are always selected and
    generated by the tool.
    
    Co-authored-by: Ken Sloat <ken.s@variscite.com>
    Signed-off-by: Nate Drude <nate.d@variscite.com>
    Signed-off-by: Ken Sloat <ken.s@variscite.com>

--- board/variscite/common/imx9_eeprom.c
+++ board/variscite/common/imx9_eeprom.c
@@ -14,6 +14,12 @@
 
 #include "imx9_eeprom.h"
 
+struct mx9_ddr_adjust {
+	const char * name;
+	struct dram_cfg_param * cfg_param;
+	unsigned int cfg_num;
+};
+
 static int var_eeprom_get_dev(struct udevice **devp)
 {
 	int ret;
@@ -111,6 +117,190 @@ void var_eeprom_print_prod_info(struct var_eeprom *ep)
 	printf("SOM revision: 0x%x\n", ep->somrev);
 	printf("DRAM PN: VIC-%04d\n", ep->ddr_vic);
 	debug("DRAM size: %d GiB\n\n", (ep->dramsize * 128) / 1024);
+}
+#endif
+
+#if defined(CONFIG_SPL_BUILD)
+static int var_eeprom_crc32(struct var_eeprom *ep, const uint32_t offset,
+							const uint32_t len, uint32_t * crc32_val) {
+	uint32_t i;
+	struct udevice *dev;
+	int ret;
+
+	/* No data in EEPROM - return -1 */
+	if (!var_eeprom_is_valid(ep)) {
+		return -1;
+	}
+
+	ret = var_eeprom_get_dev(&dev);
+	if (ret) {
+		debug("%s: Failed 2 to detect I2C EEPROM\n", __func__);
+		return ret;
+	}
+
+	*crc32_val = crc32(0, NULL, 0);
+	for (i = 0; i < len; i++) {
+		uint8_t data;
+		dm_i2c_read(dev, offset + i, &data, 1);
+		*crc32_val = crc32(*crc32_val, &data, 1);
+	}
+
+	debug("%s: crc32=0x%08x (offset=%d len=%d)\n", __func__, *crc32_val, offset, len);
+
+	return 0;
+}
+
+/*
+ * Modify DRAM table based on adjustment table in EEPROM
+ *
+ * Assumption: register addresses in the adjustment table
+ * follow the order of register addresses in the original table
+ *
+ * @adj_table_offset - offset of adjustment table from start of EEPROM
+ * @adj_table_size   - number of rows in adjustment table
+ * @table            - pointer to DDR table
+ * @table_size       - number of rows in DDR table
+ */
+static void adjust_dram_table(u16 adj_table_offset, u16 adj_table_size,
+				struct dram_cfg_param *table, u16 table_size)
+{
+	int i, j = 0;
+	u16 off = adj_table_offset;
+	struct dram_cfg_param adj_table_row;
+	int ret;
+	struct udevice *dev;
+
+	/* Get EEPROM device */
+	ret = var_eeprom_get_dev(&dev);
+	if (ret) {
+		debug("%s: Failed to detect I2C EEPROM\n", __func__);
+		return;
+	}
+
+	/* Iterate over adjustment table */
+	for (i = 0; i < adj_table_size; i++) {
+		/* Read next entry from adjustment table */
+		dm_i2c_read(dev, off,
+			(uint8_t *)&adj_table_row, sizeof(adj_table_row));
+
+		/* Iterate over DDR table and adjust it */
+		for (; j < table_size; j++) {
+			if (table[j].reg == adj_table_row.reg) {
+				debug("Adjusting reg=0x%x val=0x%x\n",
+					adj_table_row.reg, adj_table_row.val);
+				table[j].val = adj_table_row.val;
+				break;
+			}
+		}
+
+		off += sizeof(adj_table_row);
+	}
+}
+
+/*
+ * Modify DRAM tables based on adjustment tables in EEPROM
+ *
+ * @e - pointer to EEPROM header structure
+ * @d - pointer to DRAM configuration structure
+  */
+void var_eeprom_adjust_dram(struct var_eeprom *ep, struct dram_timing_info *d)
+{
+	int i;
+	u16 adj_table_size[DRAM_TABLE_NUM];
+	u32 ddr_crc32, ddr_adjust_bytes = 0;
+
+	/* Aligned with Variscite SoM EEPROM DDR Adjust Tables */
+	struct mx9_ddr_adjust mx9_adjust_table[] = {
+		{ .name = "DDRC",	.cfg_param = d->ddrc_cfg,		.cfg_num = d->ddrc_cfg_num },
+		{ .name = "DDR PHY",	.cfg_param = d->ddrphy_cfg,		.cfg_num = d->ddrphy_cfg_num },
+		{ .name = "PIE",	.cfg_param = d->ddrphy_pie,		.cfg_num = d->ddrphy_pie_num },
+		{ .name = "FSP_CFG[0].ddrc_cfg",
+			.cfg_param = d->fsp_cfg[0].ddrc_cfg,	.cfg_num =  ARRAY_SIZE(d->fsp_cfg[0].ddrc_cfg)},
+		{ .name = "FSP_CFG[0].mr_cfg",
+			.cfg_param = d->fsp_cfg[0].mr_cfg,	.cfg_num =  ARRAY_SIZE(d->fsp_cfg[0].mr_cfg)},
+		{ .name = "FSP_CFG[1].ddrc_cfg",
+			.cfg_param = d->fsp_cfg[1].ddrc_cfg,	.cfg_num =  ARRAY_SIZE(d->fsp_cfg[1].ddrc_cfg)},
+		{ .name = "FSP_CFG[1].mr_cfg",
+			.cfg_param = d->fsp_cfg[1].mr_cfg,	.cfg_num =  ARRAY_SIZE(d->fsp_cfg[1].mr_cfg)},
+		{ .name = "FSP_CFG[2].ddrc_cfg",
+			.cfg_param = d->fsp_cfg[2].ddrc_cfg,	.cfg_num =  ARRAY_SIZE(d->fsp_cfg[2].ddrc_cfg)},
+		{ .name = "FSP_CFG[2].mr_cfg",
+			.cfg_param = d->fsp_cfg[2].mr_cfg,	.cfg_num =  ARRAY_SIZE(d->fsp_cfg[2].mr_cfg)},
+		{ .name = "FSP0",	.cfg_param = d->fsp_msg[0].fsp_cfg, 	.cfg_num = d->fsp_msg[0].fsp_cfg_num },
+		{ .name = "FSP0 2D",	.cfg_param = d->fsp_msg[3].fsp_cfg, 	.cfg_num = d->fsp_msg[3].fsp_cfg_num },
+		{ .name = "FSP1",	.cfg_param = d->fsp_msg[1].fsp_cfg, 	.cfg_num = d->fsp_msg[1].fsp_cfg_num },
+		{ .name = "FSP2",	.cfg_param = d->fsp_msg[2].fsp_cfg, 	.cfg_num = d->fsp_msg[2].fsp_cfg_num },
+		{ .name = NULL },
+	};
+
+	if (!var_eeprom_is_valid(ep))
+		return;
+
+	/* Check EEPROM version - only version 2+ has DDR adjustment tables */
+	if (ep->version < 2) {
+		debug("EEPROM version is %d\n", ep->version);
+		return;
+	}
+
+	debug("EEPROM offset table\n");
+	for (i = 0; i < DRAM_TABLE_NUM + 1; i++)
+		debug("off[%d]=%d\n", i, ep->off[i]);
+
+	/* Calculate DRAM adjustment table sizes */
+	for (i = 0; i < DRAM_TABLE_NUM && ep->off[i + 1] != 0; i++) {
+		adj_table_size[i] = (ep->off[i + 1] - ep->off[i]) /
+				(sizeof(struct dram_cfg_param));
+
+		/* Calculate the total size of the ddr adjust tables */
+		ddr_adjust_bytes += ep->off[i + 1] - ep->off[i];
+	}
+
+	debug("\nSizes table\n");
+	for (i = 0; i < DRAM_TABLE_NUM; i++)
+		debug("sizes[%d]=%d\n", i, adj_table_size[i]);
+
+	/* Calculate DDR Adjust table CRC32 */
+	if (var_eeprom_crc32(ep, ep->off[0], ddr_adjust_bytes, &ddr_crc32)) {
+		printf("%s: Error: DDR adjust table crc calculation failed\n", __func__);
+		return;
+	}
+
+	/* Verify DDR Adjust table CRC32 */
+	if (ddr_crc32 != ep->ddr_crc32) {
+		printf("%s: Error: DDR adjust table invalid CRC "
+			"eeprom=0x%08x, calculated=0x%08x, len=%d\n",
+			__func__, ep->ddr_crc32, ddr_crc32, ddr_adjust_bytes);
+		return;
+	}
+	debug("crc32: eeprom=0x%08x, calculated=0x%08x, len=%d\n", ep->ddr_crc32, ddr_crc32, ddr_adjust_bytes);
+
+	/* Adjust all DDR Tables */
+	for (i = 0; mx9_adjust_table[i].name != NULL; i++)
+	{
+		debug("\nAdjusting %s table: offset=%d, count=%d\n",
+			mx9_adjust_table[i].name, ep->off[i], adj_table_size[i]);
+		adjust_dram_table(ep->off[i], adj_table_size[i],
+			mx9_adjust_table[i].cfg_param, mx9_adjust_table[i].cfg_num);
+	}
+
+	/* Adjust FSP rates and PLL bypass */
+	for (i = 0; i < NUM_FSPS; i++) {
+		debug("\nAdjusting fsp_msg[%d].drate to %d\n", i, ep->fsp_drate[i]);
+		d->fsp_msg[i].drate = ep->fsp_drate[i];
+		d->fsp_table[i] = ep->fsp_drate[i];
+
+		if (i == 0) {
+			/* Last FSP is also for the primary frequency (2d version) */
+			d->fsp_table[NUM_FSPS] = ep->fsp_drate[0];
+		}
+
+		if (ep->fsp_bypass & (1 << i))
+			d->fsp_cfg[i].bypass = 1;
+		else
+			d->fsp_cfg[i].bypass = 0;
+
+		debug("\nAdjusting fsp_cfg[%d].bypass to %d\n", i, d->fsp_cfg[i].bypass);
+	}
 }
 #endif
 
