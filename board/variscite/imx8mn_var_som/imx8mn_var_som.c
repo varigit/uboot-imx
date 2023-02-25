4f4253943f37223d9d216dba29dcb85b85efd703
Diego Dorta <diego.d@variscite.com>
2023-02-25 11:38:22 -0300

    Add support for Variscite's VAR-SOM-MX8M-NANO
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ board/variscite/imx8mn_var_som/imx8mn_var_som.c
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2018 NXP
+ * Copyright 2019-2023 Variscite Ltd.
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <env.h>
+#include <init.h>
+#include <asm/global_data.h>
+#include <init.h>
+#include <miiphy.h>
+#include <netdev.h>
+#include <asm/mach-imx/iomux-v3.h>
+#include <asm-generic/gpio.h>
+#include <asm/arch/imx8mn_pins.h>
+#include <asm/arch/clock.h>
+#include <asm/arch/sys_proto.h>
+#include <asm/mach-imx/gpio.h>
+#include <asm/mach-imx/mxc_i2c.h>
+#include <i2c.h>
+#include <asm/io.h>
+#include <usb.h>
+#include <imx_sip.h>
+#include <linux/arm-smccc.h>
+
+#include "../common/extcon-ptn5150.h"
+#include "../common/imx8_eeprom.h"
+#include "imx8mn_var_som.h"
+
+int var_setup_mac(struct var_eeprom *eeprom);
+
+DECLARE_GLOBAL_DATA_PTR;
+
+#define UART_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_FSEL1)
+#define WDOG_PAD_CTRL	(PAD_CTL_DSE6 | PAD_CTL_ODE | PAD_CTL_PUE | PAD_CTL_PE)
+
+static iomux_v3_cfg_t const uart4_pads[] = {
+	IMX8MN_PAD_UART4_RXD__UART4_DCE_RX | MUX_PAD_CTRL(UART_PAD_CTRL),
+	IMX8MN_PAD_UART4_TXD__UART4_DCE_TX | MUX_PAD_CTRL(UART_PAD_CTRL),
+};
+
+static iomux_v3_cfg_t const wdog_pads[] = {
+	IMX8MN_PAD_GPIO1_IO02__WDOG1_WDOG_B  | MUX_PAD_CTRL(WDOG_PAD_CTRL),
+};
+
+int board_early_init_f(void)
+{
+	struct wdog_regs *wdog = (struct wdog_regs *)WDOG1_BASE_ADDR;
+
+	imx_iomux_v3_setup_multiple_pads(wdog_pads, ARRAY_SIZE(wdog_pads));
+
+	set_wdog_reset(wdog);
+
+	imx_iomux_v3_setup_multiple_pads(uart4_pads, ARRAY_SIZE(uart4_pads));
+
+	init_uart_clk(3);
+
+	return 0;
+}
+
+#ifdef CONFIG_FEC_MXC
+static void setup_fec(void)
+{
+	struct iomuxc_gpr_base_regs *const iomuxc_gpr_regs
+		= (struct iomuxc_gpr_base_regs *) IOMUXC_GPR_BASE_ADDR;
+
+	/* Use 125M anatop REF_CLK1 for ENET1, not from external */
+	clrsetbits_le32(&iomuxc_gpr_regs->gpr[1],
+			IOMUXC_GPR_GPR1_GPR_ENET1_TX_CLK_SEL_SHIFT, 0);
+}
+#endif
+
+#ifdef CONFIG_CI_UDC
+
+#ifdef CONFIG_EXTCON_PTN5150
+static struct extcon_ptn5150 usb_ptn5150;
+#endif
+
+int board_usb_init(int index, enum usb_init_type init)
+{
+	imx8m_usb_power(index, true);
+
+#if (!defined(CONFIG_SPL_BUILD) && defined(CONFIG_EXTCON_PTN5150))
+	if (index == 0) {
+		/* Verify port is in proper mode */
+		int phy_mode = extcon_ptn5150_phy_mode(&usb_ptn5150);
+
+		//Only verify phy_mode if ptn5150 is initialized
+		if (phy_mode >= 0 && phy_mode != init)
+			return -ENODEV;
+	}
+#endif
+
+	return 0;
+}
+
+int board_usb_cleanup(int index, enum usb_init_type init)
+{
+	imx8m_usb_power(index, false);
+
+	return 0;
+}
+
+#ifdef CONFIG_EXTCON_PTN5150
+int board_ehci_usb_phy_mode(struct udevice *dev)
+{
+	int usb_phy_mode = extcon_ptn5150_phy_mode(&usb_ptn5150);
+
+	/* Default to host mode if not connected */
+	if (usb_phy_mode < 0)
+		usb_phy_mode = USB_INIT_HOST;
+
+	return usb_phy_mode;
+}
+#endif
+#endif
+
+int board_init(void)
+{
+	if (CONFIG_IS_ENABLED(FEC_MXC)) {
+		setup_fec();
+	}
+
+	return 0;
+}
+
+int var_get_som_rev(struct var_eeprom *ep)
+{
+	switch (ep->somrev) {
+	case 0:
+		return SOM_REV_10;
+	case 1:
+		return SOM_REV_11;
+	case 2:
+		return SOM_REV_12;
+	case 3:
+		return SOM_REV_13;
+	default:
+		return UNKNOWN_REV;
+	}
+}
+
+#define SDRAM_SIZE_STR_LEN 5
+
+int board_late_init(void)
+{
+	int som_rev;
+	struct var_eeprom *ep = VAR_EEPROM_DATA;
+	char sdram_size_str[SDRAM_SIZE_STR_LEN];
+	struct var_carrier_eeprom carrier_eeprom;
+	char carrier_rev[CARRIER_REV_LEN] = {0};
+
+	if (CONFIG_IS_ENABLED(EXTCON_PTN5150)) {
+		extcon_ptn5150_setup(&usb_ptn5150);
+	}
+
+
+#ifdef CONFIG_FEC_MXC
+	var_setup_mac(ep);
+#endif
+	var_eeprom_print_prod_info(ep);
+
+	som_rev = var_get_som_rev(ep);
+
+	printf("board_late_init: som_rev=%d\n", som_rev);
+
+	snprintf(sdram_size_str, SDRAM_SIZE_STR_LEN, "%d",
+		(int) (gd->ram_size / 1024 / 1024));
+	env_set("sdram_size", sdram_size_str);
+
+	env_set("board_name", "VAR-SOM-MX8M-NANO");
+	switch (som_rev) {
+	case SOM_REV_10:
+		env_set("som_rev", "som_rev10");
+		break;
+	case SOM_REV_11:
+		env_set("som_rev", "som_rev11");
+		break;
+	case SOM_REV_12:
+		env_set("som_rev", "som_rev12");
+		break;
+	case SOM_REV_13:
+		env_set("som_rev", "som_rev13");
+		break;
+	}
+	var_carrier_eeprom_read(CARRIER_EEPROM_BUS, CARRIER_EEPROM_ADDR, &carrier_eeprom);
+	var_carrier_eeprom_get_revision(&carrier_eeprom, carrier_rev, sizeof(carrier_rev));
+	env_set("carrier_rev", carrier_rev);
+
+#ifdef CONFIG_ENV_IS_IN_MMC
+	board_late_mmc_env_init();
+#endif
+
+	return 0;
+}
+
+#ifdef CONFIG_FSL_FASTBOOT
+#ifdef CONFIG_ANDROID_RECOVERY
+int is_recovery_key_pressing(void)
+{
+       return 0; /*TODO*/
+}
+#endif /*CONFIG_ANDROID_RECOVERY*/
+#endif /*CONFIG_FSL_FASTBOOT*/
