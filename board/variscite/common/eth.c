76c27b8313aa3e463f788a29090bbd97eec4da89
Nate Drude <nate.d@variscite.com>
2023-05-16 14:22:04 -0500

    variscite: eth: print warning if enetaddr or enet1addr exists
    Variscite SOMs feature an EEPROM with one or two MAC addresses that are
    used by default. They can be overridden by setting ethaddr or eth1addr
    in the U-Boot environment.
    
    Print a warning if the MAC address(es) in the environment do not match
    the EEPROM. For example:
    
      => setenv ethaddr f8:dc:7a:7a:1d:dc
      => saveenv && reset
      ..
      Warning: eth0 MAC addresses don't match:
      Address in EEPROM is            f8:dc:7a:7a:1d:da
      Address in environment is       f8:dc:7a:7a:1d:dc
    
    Cherry-picked from a0f8f5f6f642f9e6c7c9e240dfabfd80167f470c
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- board/variscite/common/eth.c
+++ board/variscite/common/eth.c
@@ -36,6 +36,55 @@ static void int2mac(const uint64_t mac, uint8_t *hwaddr)
 }
 #endif
 
+/**
+ * Set the MAC address in the environment and print a warning if
+ * it already exists
+ *
+ * @param interface	Ethernet interface, 0 (=>eth0),1 (=>eth1) ...
+ * @param enetaddr	MAC address value to assign to ethernet interface
+ * @return 		0 if ok, 1 on error
+ */
+static int var_eth_env_set_enetaddr(const uint8_t interface,
+                                    const uint8_t *enetaddr)
+{
+	int ret;
+	char name[12] = "enetaddr";
+	uint8_t enetaddr_env[ARP_HLEN];
+
+	/* Initialize the environment variable name based on the interface */
+	if (interface == 0)
+		snprintf(name, sizeof(name), "ethaddr");
+	else
+		snprintf(name, sizeof(name), "eth%daddr", interface);
+
+	/* Try to update the environment variable */
+	ret = eth_env_set_enetaddr(name, enetaddr);
+
+	/* If the variable already exists, read it and print a warning */
+	if (ret == -EEXIST)
+	{
+		eth_env_get_enetaddr(name, enetaddr_env);
+
+		if (memcmp(enetaddr, enetaddr_env, ARP_HLEN) != 0)
+		{
+			printf("Warning: eth%d MAC addresses don't match:\n",
+			       interface);
+			printf("Address in EEPROM is\t\t%pM\n", enetaddr);
+			printf("Address in environment is \t%pM\n",
+			       enetaddr_env);
+		}
+	}
+
+	return ret;
+}
+
+/**
+ * Read MAC address(es) from Variscite EEPROM and set the MAC address(es)
+ * in the U-Boot environment.
+ *
+ * @param eeprom pointer to EEPROM Structure
+ * @return 0 if ok, 1 on error
+ */
 int var_setup_mac(struct var_eeprom *eeprom)
 {
 	int ret;
@@ -46,23 +95,23 @@ int var_setup_mac(struct var_eeprom *eeprom)
 	uint8_t enet1addr[ARP_HLEN];
 #endif
 
-	ret = eth_env_get_enetaddr("ethaddr", enetaddr);
-	if (ret)
-		return 0;
-
+	/* Read MAC address from EEPROM */
 	ret = var_eeprom_get_mac(eeprom, enetaddr);
 	if (ret)
 		return ret;
 
+	/* Make sure EEPROM address is valid */
 	if (!is_valid_ethaddr(enetaddr))
 		return -1;
 
-	eth_env_set_enetaddr("ethaddr", enetaddr);
+	/* Set eth0 MAC address */
+	var_eth_env_set_enetaddr(0, enetaddr);
 
 #if defined(CONFIG_ARCH_IMX8) || defined(CONFIG_IMX8MP) || defined(CONFIG_IMX93)
+	/* Set eth1 MAC address to eth0 + 1 */
 	addr = mac2int(enetaddr);
 	int2mac(addr + 1, enet1addr);
-	eth_env_set_enetaddr("eth1addr", enet1addr);
+	var_eth_env_set_enetaddr(1, enet1addr);
 #endif
 
 	return 0;
