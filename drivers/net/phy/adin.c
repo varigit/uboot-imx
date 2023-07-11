609418ce290e5340618a0e84a3071fb4a4901d26
Nate Drude <nate.d@variscite.com>
2023-07-11 14:32:32 -0500

    phy: adin: add support for reading extended registers with mdio cmd
    The adin phy has extended registers that can be accessed using
    adin_ext_read and adin_ext_write. These registers can be read directly
    using the mdio command using readext and writext. For example:
    
        => mdio rx ethernet@428a0000 0xff23
        Reading from bus ethernet@428a0000
        PHY at address 0:
        65315 - 0xe01
    
    # Conflicts:
    #       drivers/net/phy/adin.c
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>
    (cherry picked from commit d68fdfbc1229d3468d099e39c8789c6dda10204b)
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>
    (cherry picked from commit e7df4fea75223bcef6f53bfc096d3a7691ddb51c)

--- drivers/net/phy/adin.c
+++ drivers/net/phy/adin.c
@@ -182,6 +182,18 @@ static int adin_config_clk_out(struct phy_device *phydev)
 			      ADIN1300_GE_CLK_CFG_MASK & sel);
 }
 
+static int adin_extread(struct phy_device *phydev, int addr, int devaddr,
+			       int regnum)
+{
+	return adin_ext_read(phydev, regnum);
+}
+
+static int adin_extwrite(struct phy_device *phydev, int addr,
+				int devaddr, int regnum, u16 val)
+{
+	return adin_ext_write(phydev, regnum, val);
+}
+
 static int adin_config_rgmii_mode(struct phy_device *phydev)
 {
 	u16 reg_val;
@@ -260,6 +272,8 @@ static struct phy_driver ADIN1300_driver =  {
 	.config = adin1300_config,
 	.startup = genphy_startup,
 	.shutdown = genphy_shutdown,
+	.readext = adin_extread,
+	.writeext = adin_extwrite,
 };
 
 int phy_adin_init(void)
