dd5e0fd74cf5689dc40bc6956edf14ef75567ffc
Alifer Moraes <alifer.m@variscite.com>
2023-07-11 23:09:14 -0300

    configs: drop CONFIG_SPL_ABORT_ON_RAW_IMAGE
    CONFIG_SPL_RAW_IMAGE_SUPPORT default y has been used to replace
    CONFIG_SPL_ABORT_ON_RAW_IMAGE for quite some time, so drop
    CONFIG_SPL_ABORT_ON_RAW_IMAGE.
    
    Derived from: nxp-imx/uboot-imx@ca3369df71d8
    
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

--- include/configs/imx8mp_var_dart.h
+++ include/configs/imx8mp_var_dart.h
@@ -17,9 +17,6 @@
 
 #ifdef CONFIG_SPL_BUILD
 
-/* For RAW image gives a error info not panic */
-#define CONFIG_SPL_ABORT_ON_RAW_IMAGE
-
 #define CONFIG_SYS_I2C
 #define CONFIG_SYS_I2C_SPEED		100000
 #endif
