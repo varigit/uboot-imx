f09145bf90b9eecda9a6df45491ffc8cddc01c22
Ye Li <ye.li@nxp.com>
2023-10-20 19:32:21 +0800

    LFU-595-1 mtd: nand: spi: Add W25N02KW SPI NAND flash
    Add Winbond W25N02KW SPI NAND flash support. The flash is 256MB and
    has new oob layout with 8bit ECC correction. Add a new get_ecc_status
    implementation to get max bit flip count.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    Reviewed-by: Han Xu <han.xu@nxp.com>

--- drivers/mtd/nand/spi/winbond.c
+++ drivers/mtd/nand/spi/winbond.c
@@ -13,6 +13,7 @@
 #endif
 #include <linux/bug.h>
 #include <linux/mtd/spinand.h>
+#include <linux/bug.h>
 
 #define SPINAND_MFR_WINBOND		0xEF
 
@@ -106,12 +107,12 @@ static const struct mtd_ooblayout_ops w25n02kv_ooblayout = {
 	.rfree = w25n02kv_ooblayout_free,
 };
 
-static int w25n02kv_ecc_get_status(struct spinand_device *spinand,
-				   u8 status)
+static int w25n02kv_ecc_get_status(struct spinand_device *spinand, u8 status)
 {
-	struct nand_device *nand = spinand_to_nand(spinand);
 	u8 mbf = 0;
-	struct spi_mem_op op = SPINAND_GET_FEATURE_OP(0x30, &mbf);
+	struct spi_mem_op op = SPINAND_GET_FEATURE_OP(0x30, spinand->scratchbuf);
+	struct nand_device *nand = spinand_to_nand(spinand);
+	int ret;
 
 	switch (status & STATUS_ECC_MASK) {
 	case STATUS_ECC_NO_BITFLIPS:
@@ -126,10 +127,11 @@ static int w25n02kv_ecc_get_status(struct spinand_device *spinand,
 		 * in order to avoid forcing the wear-leveling layer to move
 		 * data around if it's not necessary.
 		 */
-		if (spi_mem_exec_op(spinand->slave, &op))
-			return nand->eccreq.strength;
+		ret = spi_mem_exec_op(spinand->slave, &op);
+		if (ret)
+			return ret;
 
-		mbf >>= 4;
+		mbf = *spinand->scratchbuf >> 4;
 
 		if (WARN_ON(mbf > nand->eccreq.strength || !mbf))
 			return nand->eccreq.strength;
@@ -165,6 +167,15 @@ static const struct spinand_info winbond_spinand_table[] = {
 		     SPINAND_ECCINFO(&w25m02gv_ooblayout, NULL)),
 	SPINAND_INFO("W25N02KV",
 		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0xaa, 0x22),
+		     NAND_MEMORG(1, 2048, 128, 64, 2048, 40, 1, 1, 1),
+		     NAND_ECCREQ(8, 512),
+		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
+					      &write_cache_variants,
+					      &update_cache_variants),
+		     0,
+		     SPINAND_ECCINFO(&w25n02kv_ooblayout, w25n02kv_ecc_get_status)),
+	SPINAND_INFO("W25N02KW",
+		     SPINAND_ID(SPINAND_READID_METHOD_OPCODE_DUMMY, 0xba, 0x22),
 		     NAND_MEMORG(1, 2048, 128, 64, 2048, 40, 1, 1, 1),
 		     NAND_ECCREQ(8, 512),
 		     SPINAND_INFO_OP_VARIANTS(&read_cache_variants,
