3d172741c9a650ac30ebe448888a88cfa45e8454
Alexander Herfurtner <alexander.h@variscite.com>
2023-12-06 15:44:15 +0100

    imx93_var_som: Add support for Variscite VAR-SOM-MX93
    Checked out from: 75c2df1f1f
    
    Squashed history:
    120b9de3e9 imx93_var_som: Add support for Variscite VAR-SOM-MX93
    485ec5858e imx93_var_som: dts: Correct USDHC CLK and STROBE pad config settings
    823965adba imx93_var_som: dts: Disable UHS, HS200, and HS400 MMC modes for SD card
    09356ce116 imx93_var_som: dts: Disable card detect pin for SD card
    20bf34779e imx93_var_som: Set CONFIG_BOOTDELAY=1 by default
    72c33ce5d0 imx93_var_som: dts: Re-enable UHS, HS200, and HS400 MMC modes for SD card
    1b8dce3ca8 imx93_var_som: dts: Add a 10 ms start up delay to the SD card regulator
    8a1b12525e imx93_var_som: Replace NXP runtime config vars with Variscite's
    618b88acbf imx93_var_som: Change MMC env offset to 0x700000
    45122eef24 mx93_var_som: Update base lpddr4 timings for multiple FSPs
    1c4de28808 variscite: imx9_eeprom: Adjust DDR timings from EEPROM
    7730da3a51 imx93_var_som: s400: start the S400 RNG at boot
    2ac6193cd5 imx93_var_som: Fix default boot.scr handling
    9b2d30bbda imx93_var_som: Add support for uEnv.txt import
    c1e7453b15 arm: dts: imx93-var-som: drop at803x,eee-disabled
    2563592ee7 imx93-var-som: add support for MaxLinear mxl8611x Ethernet PHY
    e982e7435c imx93_var_som: conditionally set cma size using sdram size
    b2d3dfba87 imx93_var_som: Add support for M33 boot commands
    67ede4ccda imx93_var_som: fix som_rev environment variable format
    43770e21ba imx93_var_som: conditionally boot linux dts according to som_rev
    7c7911edbd imx93_var_som: fix compile warnings
    397bd175a4 imx93_var_som: add support for booting wbe device tree
    65bb732a30 imx93_var_som: Correct SPL uses of DWC_ETH_QOS
    23a12fd49f imx93_var_som: Drop support for Rev. 1.x SoMs
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>
    Co-authored-by: Ken Sloat <ken.s@variscite.com>
    Co-authored-by: Leonid Segal <leonids@variscite.com>
    Co-authored-by: Nate Drude <nate.d@variscite.com>

---
+++ include/configs/imx93_var_som.h
@@ -0,0 +1,220 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2022 NXP
+ * Copyright 2023 Variscite Ltd.
+ */
+
+#ifndef __IMX93_VAR_SOM_H
+#define __IMX93_VAR_SOM_H
+
+#include <linux/sizes.h>
+#include <linux/stringify.h>
+#include <asm/arch/imx-regs.h>
+#include "imx_env.h"
+
+#define CFG_SYS_UBOOT_BASE	\
+	(QSPI0_AMBA_BASE + CONFIG_SYS_MMCSD_RAW_MODE_U_BOOT_SECTOR * 512)
+
+#ifdef CONFIG_AHAB_BOOT
+#define AHAB_ENV "sec_boot=yes\0"
+#else
+#define AHAB_ENV "sec_boot=no\0"
+#endif
+
+#ifdef CONFIG_DISTRO_DEFAULTS
+#define BOOT_TARGET_DEVICES(func) \
+	func(MMC, mmc, 0) \
+	func(MMC, mmc, 1) \
+	func(USB, usb, 0)
+
+#include <config_distro_bootcmd.h>
+#else
+#define BOOTENV
+#endif
+
+#define CFG_MFG_ENV_SETTINGS \
+	CFG_MFG_ENV_SETTINGS_DEFAULT \
+	"initrd_addr=0x83800000\0" \
+	"initrd_high=0xffffffffffffffff\0" \
+	"emmc_dev=0\0"\
+	"sd_dev=1\0" \
+
+/* Initial environment variables */
+#define CFG_EXTRA_ENV_SETTINGS \
+	CFG_MFG_ENV_SETTINGS \
+	BOOTENV \
+	AHAB_ENV \
+	"script=boot.scr\0" \
+	"scriptaddr=0x83500000\0" \
+	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
+	"image=Image.gz\0" \
+	"img_addr=0x82000000\0" \
+	"splashimage=0x90000000\0" \
+	"console=ttyLP0,115200 earlycon\0" \
+	"fdt_addr_r=0x83000000\0" \
+	"fdt_addr=0x83000000\0" \
+	"fdt_high=0xffffffffffffffff\0" \
+	"cntr_addr=0x98000000\0" \
+	"cntr_file=os_cntr_signed.bin\0" \
+	"boot_fit=no\0" \
+	"bootdir=/boot\0" \
+	"fdt_file=undefined\0" \
+	"bootm_size=0x10000000\0" \
+	"mmcdev=" __stringify(CONFIG_SYS_MMC_ENV_DEV)"\0" \
+	"mmcpart=1\0" \
+	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
+	"mmcautodetect=yes\0" \
+	"m33_addr=0x201e0000\0" \
+	"m33_addr_auxview=0x1ffe0000\0" \
+	"m33_bin=cm_hello_world.bin.release\0" \
+	"use_m33=no\0" \
+	"loadm33bin=" \
+	         "load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${m33_bin} && " \
+	         "cp.b ${loadaddr} ${m33_addr} ${filesize};\0" \
+	"runm33bin=" \
+		"echo Booting M33 from TCM; " \
+		"stopaux; " \
+		"bootaux ${m33_addr_auxview};\0" \
+	"optargs=setenv bootargs ${bootargs} ${kernelargs};\0" \
+	"mmcargs=setenv bootargs ${jh_clk} console=${console} \
+		${cma_size} cma_name=linux,cma \
+		root=/dev/mmcblk${mmcblk}p${mmcpart} rootwait rw\0 " \
+	"bootenv=uEnv.txt\0" \
+	"loadbootscript=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${script};\0" \
+	"bootscript=echo Running bootscript from mmc ...; " \
+		"source\0" \
+	"loadbootenv=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${bootenv}\0" \
+	"importbootenv=echo Importing environment from mmc ...; " \
+		"env import -t -r $loadaddr $filesize\0" \
+	"loadimage=load mmc ${mmcdev}:${mmcpart} ${img_addr} ${bootdir}/${image};" \
+		"unzip ${img_addr} ${loadaddr}\0" \
+	"findfdt=" \
+		"if test $fdt_file = undefined; then " \
+			"if test ${som_has_wbe} = 1; then " \
+				"setenv fdt_file imx93-var-som-wbe-symphony.dtb; " \
+			"else " \
+				"setenv fdt_file imx93-var-som-symphony.dtb; " \
+				"if test ${som_rev} -lt 2; then " \
+					"echo \"Warning: Only SoM revisions >= 2.0 are supported\"; " \
+				"fi; " \
+			"fi; " \
+		"fi; " \
+		"echo fdt_file=${fdt_file};\0" \
+	"loadfdt=run findfdt;load mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${bootdir}/${fdt_file}\0" \
+	"loadcntr=fatload mmc ${mmcdev}:${mmcpart} ${cntr_addr} ${cntr_file}\0" \
+	"auth_os=auth_cntr ${cntr_addr}\0" \
+	"boot_os=booti ${loadaddr} - ${fdt_addr_r};\0" \
+	"mmcboot=echo Booting from mmc ...; " \
+		"run mmcargs; " \
+		"run optargs; " \
+		"if test ${sec_boot} = yes; then " \
+			"if run auth_os; then " \
+				"run boot_os; " \
+			"else " \
+				"echo ERR: failed to authenticate; " \
+			"fi; " \
+		"else " \
+			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
+				"bootm ${loadaddr}; " \
+			"else " \
+				"if run loadfdt; then " \
+					"run boot_os; " \
+				"else " \
+					"echo WARN: Cannot load the DT; " \
+				"fi; " \
+			"fi;" \
+		"fi;\0" \
+	"netargs=setenv bootargs ${jh_clk} console=${console} " \
+		"${cma_size} cma_name=linux,cma " \
+		"root=/dev/nfs " \
+		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
+	"netboot=echo Booting from net ...; " \
+		"run netargs;  " \
+		"run optargs; " \
+		"if test ${ip_dyn} = yes; then " \
+			"setenv get_cmd dhcp; " \
+		"else " \
+			"setenv get_cmd tftp; " \
+		"fi; " \
+		"if test ${sec_boot} = yes; then " \
+			"${get_cmd} ${cntr_addr} ${cntr_file}; " \
+			"if run auth_os; then " \
+				"run boot_os; " \
+			"else " \
+				"echo ERR: failed to authenticate; " \
+			"fi; " \
+		"else " \
+			"${get_cmd} ${img_addr} ${image}; unzip ${img_addr} ${loadaddr}; " \
+			"if test ${boot_fit} = yes || test ${boot_fit} = try; then " \
+				"bootm ${loadaddr}; " \
+			"else " \
+				"run findfdt; " \
+				"if ${get_cmd} ${fdt_addr_r} ${fdt_file}; then " \
+					"run boot_os; " \
+				"else " \
+					"echo WARN: Cannot load the DT; " \
+				"fi; " \
+			"fi;" \
+		"fi;\0" \
+	"ramsize_check="\
+		"if test $sdram_size -le 512; then " \
+			"setenv cma_size cma=32M; " \
+		"else " \
+			"setenv cma_size cma=128M; " \
+		"fi;\0" \
+	"bsp_bootcmd=echo Running BSP bootcmd ...; " \
+		"run ramsize_check; " \
+		"mmc dev ${mmcdev}; if mmc rescan; then " \
+		   "if test ${use_m33} = yes && run loadm33bin; then " \
+			   "run runm33bin; " \
+		   "fi; " \
+		   "if run loadbootscript; then " \
+			   "run bootscript; " \
+		   "else " \
+			   "if run loadbootenv; then " \
+				   "echo Loaded environment from ${bootenv};" \
+				   "run importbootenv;" \
+			   "fi;" \
+			   "if test ${sec_boot} = yes; then " \
+				   "if run loadcntr; then " \
+					   "run mmcboot; " \
+				   "else run netboot; " \
+				   "fi; " \
+			    "else " \
+				   "if run loadimage; then " \
+					   "run mmcboot; " \
+				   "else run netboot; " \
+				   "fi; " \
+				"fi; " \
+		   "fi; " \
+	   "fi;"
+
+#define CFG_SYS_INIT_RAM_ADDR	0x80000000
+#define CFG_SYS_INIT_RAM_SIZE	0x200000
+
+#define CFG_SYS_SDRAM_BASE	0x80000000
+#define PHYS_SDRAM		0x80000000
+#define DEFAULT_SDRAM_SIZE	(512 * SZ_1M) /* 512MB Minimum DDR4, see get_dram_size */
+#define PHYS_SDRAM_SIZE		DEFAULT_SDRAM_SIZE /* 2GB DDR */
+#define VAR_EEPROM_DRAM_START	(PHYS_SDRAM + (DEFAULT_SDRAM_SIZE >> 1))
+#define VAR_SOM_EEPROM_I2C_NAME		"i2c@42530000"
+#define VAR_CARRIER_EEPROM_I2C_NAME	"i2c@44340000"
+
+#define CFG_SYS_FSL_USDHC_NUM	2
+
+/* Using ULP WDOG for reset */
+#define WDOG_BASE_ADDR		WDG3_BASE_ADDR
+
+#if defined(CONFIG_CMD_NET)
+#define PHY_ANEG_TIMEOUT	20000
+#endif
+
+#ifdef CONFIG_IMX_MATTER_TRUSTY
+#define NS_ARCH_ARM64	1
+#endif
+
+#ifdef CONFIG_ANDROID_SUPPORT
+#include "imx93_evk_android.h"
+#endif
+
+#endif
