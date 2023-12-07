44533b1507dc13a6b7b671f6862b0637165f052b
Alifer Moraes <alifer.m@variscite.com>
2023-12-07 09:11:34 -0300

    imx8mn_var_som: add support for uEnv.txt import
    Derived from commit 8ca7ec9603ab.
    
    Add ability to import env from uEnv.txt, following the normal methods
    and commands.
    
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

--- include/configs/imx8mn_var_som.h
+++ include/configs/imx8mn_var_som.h
@@ -103,9 +103,13 @@
 	"optargs=setenv bootargs ${bootargs} ${kernelargs};\0" \
 	"mmcargs=setenv bootargs ${mcore_clk} console=${console} " \
 		"root=/dev/mmcblk${mmcblk}p${mmcpart} rootwait rw ${cma_size} cma_name=linux,cma\0 " \
+	"bootenv=uEnv.txt\0" \
 	"loadbootscript=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${bsp_script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
+	"loadbootenv=load mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bootdir}/${bootenv}\0" \
+	"importbootenv=echo Importing environment from mmc ...; " \
+		"env import -t -r $loadaddr $filesize\0" \
 	"loadimage=load mmc ${mmcdev}:${mmcpart} ${img_addr} ${bootdir}/${image};" \
 		"unzip ${img_addr} ${loadaddr}\0" \
 	"findfdt=" \
@@ -166,6 +170,10 @@
 		"if run loadbootscript; then " \
 			"run bootscript; " \
 		"else "\
+			"if run loadbootenv; then " \
+				"echo Loaded environment from ${bootenv}; " \
+				"run importbootenv; " \
+			"fi;" \
 			"if run loadimage; then " \
 				"run mmcboot; " \
 			"else " \
