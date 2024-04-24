varigit/uboot-imx

commit 80b9c68e772a62f44a2226558b948f61a6c454ed

LFU-666: crypto/rng: imx8ulp: Confgure rng silicon characterization data
TRNG silicon characterization is done to determine correct rng
configuration under different temperature and volatage settings.
configuring dual oscillator mode as it is more robust and faster.
recommended values for dual oscillator are ENT_DLY(25000),
FRQMIN(5000), FRQMAX(10000).

Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>

drivers/crypto/fsl/jr.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0+
 /*
  * Copyright 2008-2014 Freescale Semiconductor, Inc.
- * Copyright 2018, 2021-2022 NXP
+ * Copyright 2018, 2021-2022, 2024 NXP
  *
  * Based on CAAM driver in drivers/crypto/caam in Linux
  */
@@ -671,8 +671,22 @@ static void kick_trng(u32 ent_delay, ccsr_sec_t *sec)
 	 * for the freq_mul and the limits of the interval are used to compute
 	 * rtfrqmin, rtfrqmax
 	 */
-	sec_out32(&rng->rtfreqmin, ent_delay >> 1);
-	sec_out32(&rng->rtfreqmax, ent_delay << 7);
+	if (IS_ENABLED(CONFIG_IMX8ULP)) {
+		sec_out32(&rng->rtfreqmin, RTFRQMIN);
+		sec_out32(&rng->rtfreqmax, RTFRQMAX);
+		val = sec_in32(&rng->osc2_ctl);
+		/*
+		 * OSC2_CTL: Oscillator 2 Control Register
+		 * TRNG_ENT_CTL(1-0) = 00 : OSC1 default
+		 *                     01 : dual oscillator mode
+		 * setting the dual oscillator mode in OSC2_CTL
+		 */
+		val |= OSC2_CTL_TRNG_ENT_CTL;
+		sec_out32(&rng->osc2_ctl, val);
+	} else {
+		sec_out32(&rng->rtfreqmin, ent_delay >> 1);
+		sec_out32(&rng->rtfreqmax, ent_delay << 7);
+	}
 
 	sec_out32(&rng->rtscmisc, (retries << 16) | lrun_max);
 	sec_out32(&rng->rtpkrmax, poker_max);
@@ -776,7 +790,7 @@ static int rng_init(uint8_t sec_idx, ccsr_sec_t *sec)
 		 * if worst case value for ent_dly is identified,
 		 * loop can be skipped for that platform.
 		 */
-		if (IS_ENABLED(CONFIG_MX6SX))
+		if (IS_ENABLED(CONFIG_MX6SX) || IS_ENABLED(CONFIG_IMX8ULP))
 			break;
 
 	} while ((ret == -1) && (ent_delay < RTSDCTL_ENT_DLY_MAX));

include/fsl_sec.h
@@ -3,7 +3,7 @@
  * Common internal memory map for some Freescale SoCs
  *
  * Copyright 2014 Freescale Semiconductor, Inc.
- * Copyright 2018, 2021 NXP
+ * Copyright 2018, 2021, 2024 NXP
  */
 
 #ifndef __FSL_SEC_H
@@ -59,10 +59,16 @@ struct rng4tst {
 	u32 rtpkrrng;		/* poker range register */
 #ifdef CONFIG_MX6SX
 #define RTSDCTL_ENT_DLY		12000
+#define RTSDCTL_ENT_DLY_MAX	12000
+#elif CONFIG_IMX8ULP
+#define RTSDCTL_ENT_DLY		25000
+#define RTSDCTL_ENT_DLY_MAX	25000
+#define RTFRQMIN		5000
+#define RTFRQMAX		10000
 #else
 #define RTSDCTL_ENT_DLY		3200
-#endif
 #define RTSDCTL_ENT_DLY_MAX	12800
+#endif
 	union {
 		u32 rtpkrmax;	/* PRGM=1: poker max. limit register */
 		u32 rtpkrsq;	/* PRGM=0: poker square calc. result register */
@@ -86,7 +92,10 @@ struct rng4tst {
 #define RDSTA_MASK (RDSTA_PR(1) | RDSTA_PR(0) | RDSTA_IF(1) | RDSTA_IF(0))
 #define RDSTA_SKVN 0x40000000
 	u32 rdsta;		/*RNG DRNG Status Register*/
-	u32 rsvd2[15];
+	u32 rsvd2[10];
+#define OSC2_CTL_TRNG_ENT_CTL	1	/* TRNG_ENT_CTL(1-0) = 01 : dual oscillator mode */
+	u32 osc2_ctl;		/* RNG Oscillator 2 Control Register */
+	u32 rsvd3[4];
 };
 
 /* Version registers (Era 10+) */

commit 8ae172eaa1c5624de02dca8838b3c836c124119a

LFU-666-2: crypto: fsl: fix compilation issue
error observed ‘RTFRQMIN’ undeclared (first use in this function)
sec_out32(&rng->rtfreqmin, RTFRQMIN);

fixed the error by intorducing compile time macros.

Fixes: 80b9c68e772 (LFU-666: crypto/rng: imx8ulp: Confgure rng silicon characterization data)
Signed-off-by: Gaurav Jain <gaurav.jain@nxp.com>

drivers/crypto/fsl/jr.c
@@ -671,7 +671,7 @@ static void kick_trng(u32 ent_delay, ccsr_sec_t *sec)
 	 * for the freq_mul and the limits of the interval are used to compute
 	 * rtfrqmin, rtfrqmax
 	 */
-	if (IS_ENABLED(CONFIG_IMX8ULP)) {
+#if IS_ENABLED(CONFIG_IMX8ULP)
 		sec_out32(&rng->rtfreqmin, RTFRQMIN);
 		sec_out32(&rng->rtfreqmax, RTFRQMAX);
 		val = sec_in32(&rng->osc2_ctl);
@@ -683,10 +683,10 @@ static void kick_trng(u32 ent_delay, ccsr_sec_t *sec)
 		 */
 		val |= OSC2_CTL_TRNG_ENT_CTL;
 		sec_out32(&rng->osc2_ctl, val);
-	} else {
+#else
 		sec_out32(&rng->rtfreqmin, ent_delay >> 1);
 		sec_out32(&rng->rtfreqmax, ent_delay << 7);
-	}
+#endif
 
 	sec_out32(&rng->rtscmisc, (retries << 16) | lrun_max);
 	sec_out32(&rng->rtpkrmax, poker_max);