5a66e5eda0bc391a395764ff8649b0618e0bc472
Peng Fan <peng.fan@nxp.com>
2024-04-29 12:23:58 +0800

    LFU-729 configs: imx95_evk/verdin: disable cpuidle by default
    CPUIDLE is not stable, disable it for now.
    'setenv cpuidle ' could be used to enable it again.
    
    Reviewed-by: Jacky Bai <ping.bai@nxp.com>
    Signed-off-by: Peng Fan <peng.fan@nxp.com>

--- include/configs/verdin-imx95.h
+++ include/configs/verdin-imx95.h
@@ -87,6 +87,7 @@
 	BOOTENV \
 	AHAB_ENV \
 	"prepare_mcore=setenv mcore_clk clk-imx95.mcore_booted;\0" \
+	"cpuidle=cpuidle.off=1\0" \
 	"scriptaddr=0x93500000\0" \
 	"kernel_addr_r=" __stringify(CONFIG_SYS_LOAD_ADDR) "\0" \
 	"image=Image\0" \
@@ -104,7 +105,7 @@
 	"mmcpart=1\0" \
 	"mmcroot=/dev/mmcblk1p2 rootwait rw\0" \
 	"mmcautodetect=yes\0" \
-	"mmcargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot}\0 " \
+	"mmcargs=setenv bootargs ${cpuidle} ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot}\0 " \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${script};\0" \
 	"bootscript=echo Running bootscript from mmc ...; " \
 		"source\0" \
@@ -132,7 +133,7 @@
 				"fi; " \
 			"fi;" \
 		"fi;\0" \
-	"netargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} " \
+	"netargs=setenv bootargs ${cpuidle} ${jh_clk} ${mcore_clk} console=${console} " \
 		"root=/dev/nfs " \
 		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
 	"netboot=echo Booting from net ...; " \
