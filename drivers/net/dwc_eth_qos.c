e488554b199b8d5fc613f956a06262040034da87
Natalia Kovalenko <natasha.k@variscite.com>
2024-10-02 20:07:45 +0200

    drivers: net: dwc_eth_qos: Add PHY init function on probe
    MaxLinear mxl8611x Ethernet PHY is not detected during Ethernet device
    initialization, as seen from the U-Boot log:
      Net:   Get shared mii bus on ethernet@30be0000
      ADIN1300 PHY detected at addr 5
      eth1: ethernet@30be0000, eth0: ethernet@30bf0000 [PRIME]
    
    To detect the MaxLinear mxl8611x Ethernet PHY during Ethernet device
    activation, add the phy init function to Ethernet driver probe function.
    eqos_phy_init is based on the operational function eqos_start in part of
    the PHY connection and configuration.
    
    from the U-Boot log, after applying this commit:
      Net:   MXL86110 PHY detected at addr 4
      Get shared mii bus on ethernet@30be0000
      ADIN1300 PHY detected at addr 5
      eth1: ethernet@30be0000, eth0: ethernet@30bf0000 [PRIME]
    
    Signed-off-by: Natalia Kovalenko <natasha.k@variscite.com>
    (cherry picked from commit 4732f39a6f12df1df5977e65fece9e04e7aeb6a4)

--- drivers/net/dwc_eth_qos.c
+++ drivers/net/dwc_eth_qos.c
@@ -1491,6 +1491,44 @@ static int eqos_remove_resources_stm32(struct udevice *dev)
 	return 0;
 }
 
+static int eqos_phy_init(struct eqos_priv *eqos, struct udevice *dev)
+{
+	int ret = -1;
+	int addr = eqos_get_phy_addr(eqos, dev);
+
+#ifdef DWC_NET_PHYADDR
+	addr = DWC_NET_PHYADDR;
+#endif
+	eqos->phy = phy_connect(eqos->mii, addr, dev,
+					eqos->config->interface(dev));
+
+	if (!eqos->phy) {
+		pr_err("%s: phy_connect() failed\n", dev->name);
+		return -ENODEV;
+	}
+
+	if (eqos->max_speed) {
+		ret = phy_set_supported(eqos->phy, eqos->max_speed);
+		if (ret < 0) {
+		        pr_err("%s: phy_set_supported() failed: %d", dev->name, ret);
+		        goto err_shutdown_phy;
+		}
+	}
+
+	eqos->phy->node = eqos->phy_of_node;
+	ret = phy_config(eqos->phy);
+	if (ret < 0) {
+		pr_err("%s: phy_config() failed: %d", dev->name, ret);
+		goto err_shutdown_phy;
+	}
+
+	return 0;
+
+err_shutdown_phy:
+        phy_shutdown(eqos->phy);
+        return ret;
+}
+
 static int eqos_probe(struct udevice *dev)
 {
 	struct eqos_priv *eqos = dev_get_priv(dev);
@@ -1556,6 +1594,10 @@ static int eqos_probe(struct udevice *dev)
 #ifdef CONFIG_DM_ETH_PHY
 	eth_phy_set_mdio_bus(dev, eqos->mii);
 #endif
+
+	ret = eqos_phy_init(eqos, dev);
+	if (ret < 0)
+		goto err_free_mdio;
 
 	debug("%s: OK\n", __func__);
 	return 0;
