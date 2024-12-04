d943b2f25fb9708accbd3aa1b1352f5f763e1091
Nate Drude <nate.d@variscite.com>
2024-12-04 13:55:10 +0000

    [RND-1518] ddr: imx: Add 1336 MTps rate support
    Add PLL settings for DDR 1336 MTps based on https://community.nxp.com/t5/i-MX-Processors-Knowledge-Base/iMX-8M-Mini-Register-Programming-Aid-DRAM-PLL-setting/ta-p/1112094
    
    Derived from: https://github.com/nxp-imx/uboot-imx/commit/41b0f3454b2241ee323d7d10ef168199c8ca4f60
    
    Signed-off-by: Nate Drude <nate.d@variscite.com>

--- drivers/ddr/imx/phy/ddrphy_utils.c
+++ drivers/ddr/imx/phy/ddrphy_utils.c
@@ -140,6 +140,10 @@ void ddrphy_init_set_dfi_clk(unsigned int drate)
 		dram_pll_init(MHZ(400));
 		dram_disable_bypass();
 		break;
+	case 1336:
+		dram_pll_init(MHZ(334));
+		dram_disable_bypass();
+		break;
 	case 1200:
 		dram_pll_init(MHZ(300));
 		dram_disable_bypass();
