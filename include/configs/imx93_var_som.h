2644fce79bca8986f1820d2220e777e4002b7e02
Natalia Kovalenko <natasha.k@variscite.com>
2024-12-21 22:53:48 +0100

    imx93_var_som: Update loadcntr variable in the environment
    Replace 'fatload' by 'load' command in loadcntr to be filesystem
    type independent. Update path to the signed Linux kernel image (cntr_file)
    
    Signed-off-by: Natalia Kovalenko <natasha.k@variscite.com>

--- include/configs/imx93_var_som.h
+++ include/configs/imx93_var_som.h
@@ -107,7 +107,7 @@
 	"loadfdt=run findfdt; " \
 		"echo fdt_file=${fdt_file}; " \
 		"load mmc ${mmcdev}:${mmcpart} ${fdt_addr_r} ${bootdir}/${fdt_file}\0" \
-	"loadcntr=fatload mmc ${mmcdev}:${mmcpart} ${cntr_addr} ${cntr_file}\0" \
+	"loadcntr=load mmc ${mmcdev}:${mmcpart} ${cntr_addr} ${bootdir}/${cntr_file}\0" \
 	"auth_os=auth_cntr ${cntr_addr}\0" \
 	"boot_os=booti ${loadaddr} - ${fdt_addr_r};\0" \
 	"mmcboot=echo Booting from mmc ...; " \
