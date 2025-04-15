71a02a19ec68de3b7fd502ab549f383ddff226d9
Andre Morishita <andre.m@variscite.com>
2025-04-15 15:28:50 -0300

    imx8mn_var_som: set default nfsroot
    Setting a default NFS root directory allows to TFTP / NFS boot without
    any need to modify the environment.
    
    This allows to boot SOMs that come directly from factory. This could be
    used for all
    
    - development
    - automated testing with CI
    - flashing at customer site
    
    For this to work, the following IT setup must work:
    
    - DHCP server reports a bootstrap server. This will automatically be
    stored in the serverip field
    - The reported bootstrap server provides a TFTP server from which the
    default DTB files and the kernel image can be downloaded
    - The reported bootstrap server provides an NFS server and is exporting
    /srv/nfs/<machine-name>/rootfs
    
    Based on: 5c2d782
    
    Signed-off-by: Andre Morishita <andre.m@variscite.com>

--- include/configs/imx8mn_var_som.h
+++ include/configs/imx8mn_var_som.h
@@ -143,6 +143,7 @@
 		"else " \
 			"echo wait for boot; " \
 		"fi;\0" \
+	"nfsroot=/srv/nfs/" CONFIG_SYS_BOARD "/rootfs\0" \
 	"netargs=setenv bootargs ${mcore_clk} console=${console} " \
 		"root=/dev/nfs ${cma_size} cma_name=linux,cma " \
 		"ip=dhcp nfsroot=${serverip}:${nfsroot},v3,tcp\0" \
