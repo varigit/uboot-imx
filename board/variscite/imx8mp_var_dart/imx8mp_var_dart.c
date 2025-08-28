7e8701a5696dab3d849c7947a6e055c06374d499
Alifer Moraes <alifer.m@variscite.com>
2025-08-28 10:39:43 -0300

    imx8mp_var_dart: Remove GPIO expander 3 node for DT8MCustomBoard
    The GPIO expander 3 node was added to the imx8mp-var-dart-dt8mcustomboard
    device tree to support the new Sonata carrier board. However, this causes
    the following error in U-Boot, when using the DT8MCustomBoard:
    
      pca953x gpio@22: Error reading output register
    
    To fix this, remove the node from the device tree at runtime using
    board_fix_fdt() when the DT8MCustomBoard carrier is detected.
    
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

--- board/variscite/imx8mp_var_dart/imx8mp_var_dart.c
+++ board/variscite/imx8mp_var_dart/imx8mp_var_dart.c
@@ -356,7 +356,9 @@ int board_ehci_usb_phy_mode(struct udevice *dev)
 #ifdef CONFIG_OF_BOARD_FIXUP
 int vendor_board_fix_fdt(void *fdt_blob)
 {
+	struct var_carrier_eeprom *carrier_ep = VAR_CARRIER_EEPROM_DATA;
 	struct var_eeprom *ep = VAR_EEPROM_DATA;
+	char carrier_name[CARRIER_REV_LEN] = {0};
 	int som_rev = SOMREV_MAJOR(ep->somrev);
 
 	if (!fdt_blob) {
@@ -364,30 +366,69 @@ int vendor_board_fix_fdt(void *fdt_blob)
 		return -EINVAL;
 	}
 
-	if ((var_detect_board_id() == BOARD_ID_DART) && (som_rev >= 2)) {
-		int node_offset, subnode_offset, ret;
-		const char *node_path = "/soc@0/bus@30000000/gpio@30210000";
-		const char *node_name = "eth0_phy_pwr_hog";
+	if (var_detect_board_id() == BOARD_ID_DART) {
+		if (som_rev >= 2) {
+			/*
+			 * Remove eth0_phy_pwr_hog device tree node if DART-MX8M-PLUS
+			 * revision is 2.0 or higher.
+			 */
+			int node_offset, subnode_offset, ret;
+			const char *node_path = "/soc@0/bus@30000000/gpio@30210000";
+			const char *node_name = "eth0_phy_pwr_hog";
 
-		node_offset = fdt_path_offset(fdt_blob, node_path);
-		if (node_offset < 0) {
-			printf("WARNING: couldn't find %s: %s\n", node_path,
-			       fdt_strerror(node_offset));
-			return -ENOENT;
+			node_offset = fdt_path_offset(fdt_blob, node_path);
+			if (node_offset < 0) {
+				printf("WARNING: couldn't find %s: %s\n", node_path,
+				       fdt_strerror(node_offset));
+				return -ENOENT;
+			}
+
+			subnode_offset = fdt_subnode_offset(fdt_blob, node_offset, node_name);
+			if (subnode_offset < 0) {
+				printf("WARNING: couldn't find %s node: %s\n",
+				       node_name, fdt_strerror(subnode_offset));
+				return -ENOENT;
+			}
+
+			ret = fdt_del_node(fdt_blob, subnode_offset);
+			if (ret < 0) {
+				printf("WARNING: Couldn't delete subnode %s: %s\n",
+				       node_name, fdt_strerror(ret));
+				return ret;
+			}
 		}
 
-		subnode_offset = fdt_subnode_offset(fdt_blob, node_offset, node_name);
-		if (subnode_offset < 0) {
-			printf("WARNING: couldn't find %s node: %s\n",
-			       node_name, fdt_strerror(subnode_offset));
-			return -ENOENT;
-		}
+		if (var_carrier_eeprom_get_name(carrier_ep, carrier_name) > 0) {
+			if (strstr(carrier_name, "dt8m") || strstr(carrier_name, "legacy")) {
+				/*
+				 * Remove GPIO Expander 3 device tree node for DT8MCustomBoard,
+				 * as it is present only on the Sonata carrier board.
+				 */
+				int node_offset, subnode_offset, ret;
+				const char *node_path = "/soc@0/bus@30800000/i2c@30a30000";
+				const char *node_name = "gpio@22";
 
-		ret = fdt_del_node(fdt_blob, subnode_offset);
-		if (ret < 0) {
-			printf("WARNING: Couldn't delete subnode %s: %s\n",
-			       node_name, fdt_strerror(ret));
-			return ret;
+				node_offset = fdt_path_offset(fdt_blob, node_path);
+				if (node_offset < 0) {
+					printf("WARNING: couldn't find %s: %s\n", node_path,
+					       fdt_strerror(node_offset));
+					return -ENOENT;
+				}
+
+				subnode_offset = fdt_subnode_offset(fdt_blob, node_offset, node_name);
+				if (subnode_offset < 0) {
+					printf("WARNING: couldn't find %s node: %s\n",
+					       node_name, fdt_strerror(subnode_offset));
+					return -ENOENT;
+				}
+
+				ret = fdt_del_node(fdt_blob, subnode_offset);
+				if (ret < 0) {
+					printf("WARNING: Couldn't delete subnode %s: %s\n",
+					       node_name, fdt_strerror(ret));
+					return ret;
+				}
+			}
 		}
 	}
 
