410b788941585b75cb315e5b2bcf08d9c2be44e1
Pitchaiah P <pitchaiah.p@variscite.com>
2026-03-05 15:41:47 +0100

    imx8mn-var-som: add bootcmd_android_recovery environment variable
    Add a new U-Boot environment variable `bootcmd_android_recovery`
    to allow booting directly into Android recovery mode.
    
    The new variable runs the existing cmaargs setup and then
    boots the recovery partition using `boota recovery`.
    
    This enables easier triggering of Android recovery from
    the bootloader environment without modifying the default
    bootcmd.
    
    Signed-off-by: Pitchaiah P <pitchaiah.p@variscite.com>

--- include/configs/imx8mn_var_som_android.h
+++ include/configs/imx8mn_var_som_android.h
@@ -35,7 +35,10 @@
 #define BOOT_ENV_SETTINGS \
 	"bootcmd=" \
 		"run kernelbootargs; " \
-		"boota ${fastboot_dev}\0"
+		"boota ${fastboot_dev}\0" \
+	"bootcmd_android_recovery=" \
+		"run kernelbootargs; " \
+		"boota recovery\0"
 
 #define CFG_EXTRA_ENV_SETTINGS					\
 	HW_ENV_SETTINGS \
