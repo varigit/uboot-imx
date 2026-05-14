8785c474d2a7e95ac9a8ba0af7d2b82055718560
FrancescoFerraro <francesco.f@variscite.com>
2026-05-14 11:01:50 +0200

    mx7dvar_som: allow CONFIG_ENV_IS_NOWHERE
    Fix error when using CONFIG_ENV_IS_NOWHERE.
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- board/variscite/mx7dvar_som/mx7dvar_som.c
+++ board/variscite/mx7dvar_som/mx7dvar_som.c
@@ -418,9 +418,8 @@ int board_late_init(void)
 
 	check_emmc();
 
-#ifdef CONFIG_ENV_IS_IN_MMC
-	board_late_mmc_env_init();
-#endif
+	if (IS_ENABLED(CONFIG_MMC))
+		board_late_mmc_env_init();
 
 	snprintf(sdram_size_str, SDRAM_SIZE_STR_LEN, "%d", (int)(gd->ram_size / 1024 / 1024));
 	env_set("sdram_size", sdram_size_str);
