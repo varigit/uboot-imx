b0b865a5704da4449a95b3cd5cec03a5710e9ddc
Alexander Herfurtner <alexander.h@variscite.com>
2026-07-22 14:03:44 +0200

    mx7dvar_eeprom: Document EEPROM layout fields
    Add offset and purpose comments for the legacy and current EEPROM
    layout.
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>

--- board/variscite/mx7dvar_som/mx7dvar_eeprom.h
+++ board/variscite/mx7dvar_som/mx7dvar_eeprom.h
@@ -18,24 +18,24 @@
 #endif
 
 struct __packed mx7d_var_legacy_eeprom {
-	u32 reserved;
-	u8 part_number[16];
-	u8 assembly[16];
-	u8 date_year[4];
-	u8 date_month[3];
-	u8 date_day[2];
+	u32 reserved;		/* 00 - 0x00 - reserved */
+	u8 part_number[16];	/* 04 - 0x04 - part number suffix */
+	u8 assembly[16];	/* 20 - 0x14 - assembly number suffix */
+	u8 date_year[4];	/* 36 - 0x24 - production year */
+	u8 date_month[3];	/* 40 - 0x28 - production month */
+	u8 date_day[2];		/* 43 - 0x2b - production day */
 };
 
 struct __packed mx7d_var_eeprom {
-	u16 eeprom_magic;
-	u8 part_number[8];
-	u8 assembly[11];
-	u8 date_year[4];
-	u8 date_month[3];
-	u8 date_day[2];
-	u8 reserved;
-	u8 dram_size;		/* DRAM size in 128MB units */
-	u32 dcd_table[120];
+	u16 eeprom_magic;	/* 00 - 0x00 - magic ID, must be EEPROM_MAGIC */
+	u8 part_number[8];	/* 02 - 0x02 - part number suffix */
+	u8 assembly[11];	/* 10 - 0x0a - assembly number suffix */
+	u8 date_year[4];	/* 21 - 0x15 - production year */
+	u8 date_month[3];	/* 25 - 0x19 - production month */
+	u8 date_day[2];		/* 28 - 0x1c - production day */
+	u8 reserved;		/* 30 - 0x1e - reserved */
+	u8 dram_size;		/* 31 - 0x1f - DRAM size in 128MB units */
+	u32 dcd_table[120];	/* 32 - 0x20 - DRAM DCD register table */
 };
 
 #define VAR_EEPROM_DATA ((struct mx7d_var_eeprom *)VAR_EEPROM_DRAM_START)
