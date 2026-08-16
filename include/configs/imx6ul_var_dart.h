33bc77cff115573d815180f1dbce4a3b7589f79a
FrancescoFerraro <francesco.f@variscite.com>
2026-08-16 09:35:36 +0200

    imx6ul_var_dart: set Wi-Fi module type based on SoM revision
    Set Wi-Fi module type for VAR-SOM-6UL SOM.
    
    Signed-off-by: FrancescoFerraro <francesco.f@variscite.com>

--- include/configs/imx6ul_var_dart.h
+++ include/configs/imx6ul_var_dart.h
@@ -206,14 +206,10 @@
 				"setenv mmc0_dev sd-card; " \
 			"else " \
 				"if test \"${wifi}\" = yes; then " \
-					"if test \"${board_name}\" = VAR-SOM-6UL; then " \
-						"setenv mmc0_dev wifi; " \
+					"if test \"${som_rev}\" = '5G IW611' || test \"${som_rev}\" = '5G IW612'; then " \
+						"setenv mmc0_dev wifi-iw61x; " \
 					"else " \
-						"if test \"${som_rev}\" = '5G IW611' || test \"${som_rev}\" = '5G IW612'; then " \
-							"setenv mmc0_dev wifi-iw61x; " \
-						"else " \
-							"setenv mmc0_dev wifi-brcm; " \
-						"fi; " \
+						"setenv mmc0_dev wifi-brcm; " \
 					"fi; " \
 				"else " \
 					"setenv mmc0_dev sd-card; " \
